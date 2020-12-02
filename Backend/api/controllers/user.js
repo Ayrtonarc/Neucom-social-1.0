a3400028511e9ae42b673e3f735d0c9d2786be56
Ayrton <ayrton.arc@gmail.com>
2020-12-01 21:41:29 -0800

    Usuerios Paginados
    Se muestran los usuarios, que la web tiene con paginacion.

--- Backend/api/controllers/user.js
+++ Backend/api/controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var bcrypt = require('bcrypt-nodejs');
+var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
 var jwt = require('../services/jwt'); //se importa el servicio de JWT
@@ -115,11 +116,35 @@ function getUser(req, res){
 
     }
 
+    //Devolver lista de usuarios paginados
+    function getUsers(req, res){
+        var identity_user_id = req.user.sub;
+
+        var page = 1;
+        if(req.params.page){
+            page = req.params.page;
+        }
+        var itemsPerPage = 5;
+
+        User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+            if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+            if(!users) return res.status(404).send({message: 'No hay usuarios disponibles'});
+
+            return res.status(200).send({
+                users,
+                total,
+                pages: Math.ceil(total/itemsPerPage)
+            });
+        });
+    }
+
 module.exports = {   //todos los metodos creados se tienen que exportar en routes
     home,
     pruebas,
     saveUser,
     loginUser,
-    getUser
+    getUser,
+    getUsers
     
 }
